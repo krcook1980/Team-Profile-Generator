ccb29130b6ef71971188791cfe059ab80fc4ce81
Kelly Cook <kellyrcook1980@gmail.com>
2021-02-19 14:31:33 -0700

    working through inquirer, took them out of js files and did only in index... still working through function

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateHTML = require('./src/generateHTML');
-const employee = require('./lib/employee');
-const manager = require('./lib/manager');
-const engineer = require('./lib/engineer');
-const intern = require('./lib/intern');
+const Intern = require('./lib/intern');
 const Employee = require('./lib/employee');
 const Manager = require('./lib/manager');
 const Engineer = require('./lib/engineer');
+const newEmp = []
+
+const empQuestions = [{
+    type: 'input',
+    name: 'empName',
+    message: 'What is the employee name?'
+},
+{
+    type: 'input',
+    name: 'empId',
+    message: 'Please assign a new employee number with the format e###'
+},
+{
+    type: 'input',
+    name: 'empEmail',
+    message: 'What is the employee email address?',
+    //figure out validate if not an email address
+},
+
+]
 
 
 function nextEmp() {
-
+    Employee.prototype.getRole();
+    
     //array of questions for user
     inquirer
         .prompt([
@@ -19,35 +37,113 @@ function nextEmp() {
                 type: 'list',
                 name: 'empType',
                 message: 'What kind of employee are you entering?',
-                choices: ["Manager", "Engineer", "Intern"],
+                choices: ["Manager", "Engineer", "Intern", "Finished"],
             },
         ]).then((response) => {
-            if (response === "None") {
+            //Add employee to the newEmp array
+            newEmp.push(response.empType);
+            
+            if (response.empType === "Finished") {
                 console.log("Thank you!")
             }
-            if (response === "Manager") {
-                Employee.prototype.getName();
-                Employee.prototype.getId();
-                Employee.prototype.getEmail();
-                Manager.prototype.getOffNum();
-                Manager.prototype.getRole();
-                nextEmp();
+            else if (response.empType === "Manager") {
+                inquirer
+                .prompt([
+                    {
+                        type: 'input',
+                        name: 'empName',
+                        message: 'What is the employee name?'
+                    },
+                    {
+                        type: 'input',
+                        name: 'empId',
+                        message: 'Please assign a new employee number with the format e###:'
+                    },
+                    {
+                        type: 'input',
+                        name: 'empEmail',
+                        message: 'What is the employee email address?',
+                        //figure out validate if not an email address
+                    },
+                    {
+                        type: 'input',
+                        name: 'empNum',
+                        message: 'What is the employee office number?'
+                    },
+                ]).then((response2) => {
+                                      
+                    const newRole = Manager.prototype.getRole();
+                    newEmp.push(response2);
+                    newEmp.push(newRole);
+                    console.log(newEmp)
+                    return newEmp;
+                    //spits out manager then object of rest... not quite what i need
+                });
+
+                
+                
             }
-            else if (response === "Engineer") {
-                Employee.prototype.getName();
-                Employee.prototype.getId();
-                Employee.prototype.getEmail();
-                Engineer.prototype.getGithub();
-                Engineer.prototype.getRole();
+            else if (response.empType === "Engineer") {
+                inquirer
+                .prompt([
+                    {
+                        type: 'input',
+                        name: 'empName',
+                        message: 'What is the employee name?'
+                    },
+                    {
+                        type: 'input',
+                        name: 'empId',
+                        message: 'Please assign a new employee number with the format e###'
+                    },
+                    {
+                        type: 'input',
+                        name: 'empEmail',
+                        message: 'What is the employee email address?',
+                        //figure out validate if not an email address
+                    },
+                    {
+                        type: 'input',
+                        name: 'empLink',
+                        message: 'What is the GitHub user name for this employee?'
+                    },
+                ]).then((response) => {
+                    console.log(response);
+                    Engineer.prototype.getRole();
+                });
+                
                 nextEmp();
             }
             else {
-                Employee.prototype.getName();
-                Employee.prototype.getId();
-                Employee.prototype.getEmail();
-                Intern.prototype.getSchool();
-                Intern.prototype.getRole();
-                nextEmp();
+                inquirer
+                .prompt([
+                    {
+                        type: 'input',
+                        name: 'empName',
+                        message: 'What is the employee name?'
+                    },
+                    {
+                        type: 'input',
+                        name: 'empId',
+                        message: 'Please assign a new employee number with the format e###'
+                    },
+                    {
+                        type: 'input',
+                        name: 'empEmail',
+                        message: 'What is the employee email address?',
+                        //figure out validate if not an email address
+                    },
+                    {
+                        type: 'input',
+                        name: 'empSchool',
+                        message: 'What school did this intern attend?'
+                    },
+                ]).then((response) => {
+                    console.log(response);
+                    Intern.prototype.getRole();
+                });
+                
+               
             }
         })
 };
@@ -55,6 +151,6 @@ nextEmp();
 
 
 
-//call manager and include employee
-//prompt if engineer or intern
-//each of those needs to call employee
+
+
+
